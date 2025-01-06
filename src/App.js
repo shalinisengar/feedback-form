2f733a2e3d8c5721b22031b564c8f0939e31b913
shalinisengar <shalinisengar81@gmail.com>
2025-01-06 16:09:57 +0530

    feedback-form

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
   useEffect(() => {
     const hour = new Date().getHours();
     if (hour >= 5 && hour < 12) setGreeting("Good Morning!");
-    else if (hour >= 12 && hour < 17) setGreeting("Good Afternoon!");
-    else if (hour >= 17 && hour < 21) setGreeting("Good Evening!");
+    else if (hour >= 12 && hour < 16) setGreeting("Good Afternoon!");
+    else if (hour >= 16 && hour < 21) setGreeting("Good Evening!");
     else setGreeting("Good Night!");
 
     const savedFeedback = JSON.parse(localStorage.getItem("feedbackList")) || [];
